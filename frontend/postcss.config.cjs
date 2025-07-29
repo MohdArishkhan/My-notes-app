71ec0e684fd6e729fb84f2f61c976672a04f52b5
Mohd Arish Khan <writetoarish7@gmail.com>
2025-07-29 12:01:15 +0530

    fix: corrected postcss config for render

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,14 +1,9 @@
 // postcss.config.cjs
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
-
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
+
+
